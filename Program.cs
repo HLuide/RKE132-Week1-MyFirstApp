bd6c1bd3c21d38d8bee51feffa53a3abe431b90f
Holger Luide <holger.luide@tktk.ee>
2023-01-28 22:09:45 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada oma nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//String - sõne
+String userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//String interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
